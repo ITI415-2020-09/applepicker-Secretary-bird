1982b5d4b0f12d7eeb751d3a61ced18ed18adc2c
Secretary-bird <LLopez31098@gmail.com>
2020-09-22 23:28:08 -0400

    Finishing the score board
    Something happened with me being able to make basket recognize my ScoreCounter as text, i tried with UI text and textmeshpro, but neither worked. everything else seems to work fine however.

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour
 {
+    public Text scoreGT;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        scoreGT = scoreGO.GetComponent<Text>();
+        scoreGT.text = "0";
     }
 
     // Update is called once per frame
@@ -27,6 +31,15 @@ public class Basket : MonoBehaviour
         if (collidedWith.tag == "Apple")
         {
             Destroy(collidedWith);
+
+            int score = int.Parse(scoreGT.text);
+            score += 100;
+            scoreGT.text = score.ToString();
+
+            if (score > HighScore.score)
+            {
+                HighScore.score = score;
+            }
         }
     }
 }
